98619ec3922eeb40c0210cd78cdd34d34dd8dc65
roycms <roycms@qq.com>
2017-08-14 15:40:19 +0800

    add  cn

--- Class/TimeAgo.swift
+++ Class/TimeAgo.swift
@@ -38,7 +38,7 @@ func funStampTimeAgo(_ date: Double) -> String{
     return funTimeAgo(NSDate(timeIntervalSince1970: TimeInterval(date)) as Date)
 }
 
-func funTimeAgo(_ date: Date) -> String {
+func funTimeAgo_en(_ date: Date) -> String {
     
     let calendar = Calendar.current
     let now = Date()
@@ -100,3 +100,67 @@ func funTimeAgo(_ date: Date) -> String {
     return NSLocalizedString("JustNow",comment:"Just now")
     
 }
+func funTimeAgo(_ date: Date) -> String {
+    
+    let calendar = Calendar.current
+    let now = Date()
+    let unitFlags: NSCalendar.Unit = [.second, .minute, .hour, .day, .weekOfYear, .month, .year]
+    let components = (calendar as NSCalendar).components(unitFlags, from: date, to: now, options: [])
+    
+    if let year = components.year, year >= 2 {
+        return "\(year) 年前"
+    }
+    
+    if let year = components.year, year >= 1 {
+        return "去年"
+    }
+    
+    if let month = components.month, month >= 2 {
+        return "\(month) 月前"
+    }
+    
+    if let month = components.month, month >= 1 {
+        return "上个月"
+    }
+    
+    if let week = components.weekOfYear, week >= 2 {
+        return "\(week) 星期前"
+    }
+    
+    if let week = components.weekOfYear, week >= 1 {
+        return "上个星期"
+    }
+    
+    if let day = components.day, day >= 2 {
+        return "\(day) 天前"
+    }
+    
+    if let day = components.day, day >= 1 {
+        return "昨天"
+    }
+    
+    if let hour = components.hour, hour >= 2 {
+        return "\(hour) 小时前"
+    }
+    
+    if let hour = components.hour, hour >= 1 {
+        return "1小时前"
+    }
+    
+    if let minute = components.minute, minute >= 2 {
+        return "\(minute) 分钟前"
+    }
+    
+    if let minute = components.minute, minute >= 1 {
+        return "1分钟前"
+    }
+    
+    if let second = components.second, second >= 3 {
+        return "\(second) 秒前"
+    }
+    
+    return "刚刚"
+    
+}
+
+
